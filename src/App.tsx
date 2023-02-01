b71412cb9b392d4a7f745b0e8f9d9d3e35a465ee
Malin J <millianapia@gmail.com>
2023-02-01 14:53:55 +0100

    move project out of folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import DeckGL, { LayersList } from 'deck.gl/typed';
 import { SolidPolygon, Line, ScatterPlot, Tile, Column } from '../src/Layers';
+import style_bonn from './styles/bonn_style.json';
+import StaticMap from 'react-map-gl';
 
 const initViewState = {
   longitude: 5.313,
   latitude: 60.382,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0,
+};
+
+const initBonnViewState = {
+  longitude: 7.1003,
+  latitude: 50.741,
   zoom: 13,
   pitch: 0,
   bearing: 0,
@@ -17,7 +27,11 @@ function App() {
     ScatterPlot(),
     Column(),
   ];
-  return <DeckGL controller layers={layers} initialViewState={initViewState} />;
+  return (
+    <DeckGL controller layers={layers} initialViewState={initBonnViewState}>
+      <StaticMap mapStyle={style_bonn} />
+    </DeckGL>
+  );
 }
 
 export default App;
